42a913bbcfc600c8a3a983134971d3dd2c6b4fa0
David Poindexter <drpoindexter@gmail.com>
2019-08-01 19:29:00 -0400

    Add initial docker and gatsby site build

---
+++ public-site/src/pages/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi Heroines!</h1>
+    <p>
+      The future Hostel of the Heroines of Javascript project, powered by
+      Gatsby, Amplify, and {"<3"} from a stream community.
+    </p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
