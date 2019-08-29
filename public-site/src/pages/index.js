135db16923f7e978cd20b31f54e5e4c812f561f3
David Poindexter <drpoindexter@gmail.com>
2019-08-29 18:23:15 -0400

    Add initial meta info for gallery

--- public-site/src/pages/index.js
+++ public-site/src/pages/index.js
@@ -1,24 +1,60 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi Heroines!</h1>
+    <SEO title="Heroines Gallery Home" />
+    <h1>Hi Heroines Fans!</h1>
     <p>
-      The future Hostel of the Heroines of Javascript project, powered by
-      Gatsby, Amplify, and {"<3"} from a stream community.
+      The roberttables livestream project that presents and accessible form of
+      the limited edition 'Heroines of Javascript' card project. Powered by
+      GatsbyJS, AWS Amplify, and {"<3"} from a stream community.
     </p>
     <p>Now go build something great.</p>
+    <p>
+      If you're curious about the project, see some links here{" "}
+      <a
+        href="https://github.com/mtheoryx/heroines-of-javascript"
+        target="_blank"
+      >
+        Readme for project info
+      </a>
+    </p>
+    <hr />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      {data.Heroines.edges.map(({ node }) => (
+        <React.Fragment key={node.id}>
+          <div className="card">
+            <h2>Name: {node.frontmatter.heroine}</h2>
+            <p>Title: {node.frontmatter.title}</p>
+            <p>Card Color: {node.frontmatter.color}</p>
+          </div>
+          <hr />
+        </React.Fragment>
+      ))}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    Heroines: allMarkdownRemark(
+      sort: { fields: frontmatter___heroine, order: ASC }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            heroine
+            title
+            color
+          }
+        }
+      }
+    }
+  }
+`
