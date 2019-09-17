b4fd1d27345949df4cc7c62a921b48ee856a0882
David Poindexter <drpoindexter@gmail.com>
2019-09-17 16:56:56 -0400

    Initial navigation

--- public-site/src/pages/index.js
+++ public-site/src/pages/index.js
@@ -1,23 +1,11 @@
 import React from "react"
-import { graphql } from "gatsby"
-import Img from "gatsby-image"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import styled from "styled-components"
-
-const Card = styled.div`
-  border: 5px solid;
-  border-color: ${props => props.color};
-  border-radius: 5px;
-  margin: 5px;
-  padding: 10px;
-`
-
-const IndexPage = ({ data }) => (
+const IndexPage = () => (
   <Layout>
-    <SEO title="Heroines Gallery Home" />
-    <h1>Hi Heroines Fans!</h1>
+    <SEO title="Women in Tech Home" />
     <p>
       The roberttables livestream project that presents and accessible form of
       the limited edition 'Heroines of Javascript' card project. Powered by
@@ -35,53 +23,7 @@ const IndexPage = ({ data }) => (
       </a>
     </p>
     <hr />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      {data.Heroines.edges.map(({ node }) => (
-        <React.Fragment key={node.id}>
-          <Card color={node.frontmatter.color}>
-            <h2>{node.frontmatter.heroine}</h2>
-            <Img
-              fluid={node.frontmatter.thumbnail.childImageSharp.fluid}
-              alt={`${node.frontmatter.heroine}'s Thumbnail Illustration`}
-            />
-            <p>Title: {node.frontmatter.title}</p>
-            <p>{node.excerpt}</p>
-            <p style={{ backgroundColor: node.frontmatter.color }}>
-              Card Color: {node.frontmatter.color}
-            </p>
-          </Card>
-          <hr />
-        </React.Fragment>
-      ))}
-    </div>
   </Layout>
 )
 
 export default IndexPage
-
-export const pageQuery = graphql`
-  query {
-    Heroines: allMarkdownRemark(
-      sort: { fields: frontmatter___heroine, order: ASC }
-    ) {
-      edges {
-        node {
-          id
-          frontmatter {
-            heroine
-            title
-            color
-            thumbnail {
-              childImageSharp {
-                fluid(maxWidth: 300) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-          excerpt(truncate: true, pruneLength: 80)
-        }
-      }
-    }
-  }
-`
