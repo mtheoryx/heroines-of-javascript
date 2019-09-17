79046d19392386d1c5c2451869c5b78a4c47ee8b
David Poindexter <119065+mtheoryx@users.noreply.github.com>
2019-09-17 18:51:59 -0400

    Add slug generation for markdown pages (#15)
    * Ignore raw card design assets
    
    * Add example card markdown file
    
    * Correct Alyssa Nicoll name spelling
    
    * Add initial meta info for gallery
    
    * Change to blank template for heroine markdown file
    
    * Adds some security protections on external links
    
    * Add cards image resources
    
    * Add raw card data entry
    
    * Add hex color captures from artist original illustrations
    
    * Demo of card colors on temporary gallery page
    
    * Fix typo in Tara Manicsic's job title
    
    * Add multi-stage build to compose (#10)
    
    * Whoops, typo
    
    * Render thumbnail images
    
    * Remove default gatsby images
    
    * Fix failing build
    
    * Closes #2
    
    * Add humans.txt file
    
    * Fix incorrect heart encoding Closes #11
    
    * Initial navigation
    
    * Add simple styled component extraction
    
    * Fix styles
    
    * Add slug generation for markdown pages

---
+++ public-site/src/pages/heroines.js
@@ -0,0 +1,91 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import styled from "styled-components"
+
+const Card = styled.div`
+  border: 10px solid;
+  border-color: ${props => props.color};
+  border-radius: 15px;
+  margin: 5px;
+  padding: 10px;
+`
+
+const CardColor = styled.p`
+  background-color: ${props => props.color};
+`
+
+const HeroinesPage = ({ data }) => (
+  <Layout>
+    <SEO title="Heroines Gallery Home" />
+    <h1>Hi Heroines Fans!</h1>
+    <p>
+      The roberttables livestream project that presents and accessible form of
+      the limited edition 'Heroines of Javascript' card project. Powered by
+      GatsbyJS, AWS Amplify, and {"<3"} from a stream community.
+    </p>
+    <p>Now go build something great.</p>
+    <p>
+      If you're curious about the project, see some links here{" "}
+      <a
+        href="https://github.com/mtheoryx/heroines-of-javascript"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Readme for project info
+      </a>
+    </p>
+    <hr />
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      {data.Heroines.edges.map(({ node }) => (
+        <React.Fragment key={node.id}>
+          <Card color={node.frontmatter.color}>
+            <h2>{node.frontmatter.heroine}</h2>
+            <Img
+              fluid={node.frontmatter.thumbnail.childImageSharp.fluid}
+              alt={`${node.frontmatter.heroine}'s Thumbnail Illustration`}
+            />
+            <p>Title: {node.frontmatter.title}</p>
+            <p>{node.excerpt}</p>
+            <CardColor color={node.frontmatter.color}>
+              Card Color: {node.frontmatter.color}
+            </CardColor>
+          </Card>
+          <hr />
+        </React.Fragment>
+      ))}
+    </div>
+  </Layout>
+)
+
+export default HeroinesPage
+
+export const pageQuery = graphql`
+  query {
+    Heroines: allMarkdownRemark(
+      sort: { fields: frontmatter___heroine, order: ASC }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            heroine
+            title
+            color
+            thumbnail {
+              childImageSharp {
+                fluid(maxWidth: 300) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          excerpt(truncate: true, pruneLength: 80)
+        }
+      }
+    }
+  }
+`
---
+++ public-site/src/pages/vuevixens.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="Vue Vixens Foundation" />
+    <h1>About the Vue Vixens</h1>
+    <p>We're gonna knock your socks clean off!</p>
+  </Layout>
+)
+
+export default AboutPage
