5ccd13f761286a790af9d76cc97c8c5140678096
David Poindexter <drpoindexter@gmail.com>
2019-10-13 13:02:17 -0400

    Remove unused warnings

--- public-site/src/templates/heroine-template.js
+++ public-site/src/templates/heroine-template.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Img from "gatsby-image"
+// import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import styled from "styled-components"
+// import styled from "styled-components"
 
 const HeroinePage = ({ data }) => (
   <Layout>
