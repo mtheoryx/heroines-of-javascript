3314b6d5ab96c5b3ef5a47651a60a23988f85a4f
David Poindexter <drpoindexter@gmail.com>
2019-10-13 14:49:36 -0400

    Add some basic flexbox styles to heroines index page

--- public-site/src/pages/heroines.js
+++ public-site/src/pages/heroines.js
@@ -6,7 +6,16 @@ import SEO from "../components/seo"
 
 import styled from "styled-components"
 
+const CardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
 const Card = styled.div`
+  width: 32%;
+  max-width: 32%;
+  min-width: 200px;
   border: 10px solid;
   border-color: ${props => props.color};
   border-radius: 15px;
@@ -24,44 +33,41 @@ const CardColor = styled.p`
 const HeroinesPage = ({ data }) => (
   <Layout>
     <SEO title="Heroines Gallery Home" />
-    <h1>Hi Heroines Fans!</h1>
+    <h1>The Heroines of Javascript</h1>
     <p>
-      The roberttables livestream project that presents and accessible form of
-      the limited edition 'Heroines of Javascript' card project. Powered by
-      GatsbyJS, AWS Amplify, and {"<3"} from a stream community.
+      If you are curious who each of the heroines are, you are in the right
+      place!
     </p>
-    <p>Now go build something great.</p>
     <p>
-      If you're curious about the project, see some links here{" "}
-      <a
-        href="https://github.com/mtheoryx/heroines-of-javascript"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Readme for project info
-      </a>
+      Each of these are full-color, printed cards, issued in a set of 20. All
+      proceeds go to The Vue Vixens Scholarship Foundation. They are currently
+      sold out, but we have created this site so that you can enjoy them as
+      well!
+    </p>
+    <p>
+      Click each one to visit a page featuring photos of the front and back of
+      the physical cards in the set, social media links, and more detailed
+      information about each Heroine.
     </p>
     <hr />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <CardContainer>
       {data.Heroines.edges.map(({ node }) => (
-        <React.Fragment key={node.id}>
-          <Card color={node.frontmatter.color}>
-            <Link to={node.fields.slug}>
-              <h2>{node.frontmatter.heroine}</h2>
-              <Img
-                fluid={node.frontmatter.thumbnail.childImageSharp.fluid}
-                alt={`${node.frontmatter.heroine}'s Thumbnail Illustration`}
-              />
-              <p>Title: {node.frontmatter.title}</p>
-              <p>{node.excerpt}</p>
-              <CardColor color={node.frontmatter.color}>
-                Card Color: {node.frontmatter.color}
-              </CardColor>
-            </Link>
-          </Card>
-        </React.Fragment>
+        <Card color={node.frontmatter.color} key={node.id}>
+          <Link to={node.fields.slug}>
+            <h2>{node.frontmatter.heroine}</h2>
+            <Img
+              fluid={node.frontmatter.thumbnail.childImageSharp.fluid}
+              alt={`${node.frontmatter.heroine}'s Thumbnail Illustration`}
+            />
+            <p>Title: {node.frontmatter.title}</p>
+            <p>{node.excerpt}</p>
+            <CardColor color={node.frontmatter.color}>
+              Card Color: {node.frontmatter.color}
+            </CardColor>
+          </Link>
+        </Card>
       ))}
-    </div>
+    </CardContainer>
   </Layout>
 )
 
