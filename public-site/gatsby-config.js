869f0b2d0bed91376c1b8d47a4d5852c912c13af
David Poindexter <drpoindexter@gmail.com>
2019-10-13 18:05:39 -0400

    Add gatsby styled components plugin

--- public-site/gatsby-config.js
+++ public-site/gatsby-config.js
@@ -53,6 +53,7 @@ module.exports = {
         note: `Made in Indianapolis with <3`,
       },
     },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
