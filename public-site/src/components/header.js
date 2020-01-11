cfa5d46c879d76cd35a45bfd68023dbf5bf567dd
David Poindexter <drpoindexter@gmail.com>
2020-01-11 11:28:03 -0500

    Move nav to component, use styled-components

--- public-site/src/components/header.js
+++ public-site/src/components/header.js
@@ -1,72 +1,33 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 import styled from "styled-components"
+import Navigation from "./navigation"
 
-const navLinks = [
-  { destination: "/heroines", readable: "The Heroines" },
-  // { destination: "/vuevixens", readable: "Vue Vixens" },
-  // { destination: "/contact", readable: "Contact" },
-]
+const StyledHeader = styled.header`
+  background-color: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
 
-const Nav = ({ className }) => (
-  <nav className={className}>
-    {navLinks.map((item, index) => (
-      <Link to={item.destination} key={index} activeClassName="active">
-        {item.readable}
-      </Link>
-    ))}
-  </nav>
-)
+const HeaderWrapper = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
 
-const StyledNav = styled(Nav)`
-  a {
-    color: white;
-    text-decoration: none;
-    margin-right: 20px;
-  }
-  a:hover,
-  a.active {
-    text-decoration: underline;
-  }
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
 `
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <StyledHeader>
+    <HeaderWrapper>
       <h1 style={{ margin: 0, marginBottom: 20 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+        <StyledLink to="/">{siteTitle}</StyledLink>
       </h1>
-      <StyledNav />
-    </div>
-  </header>
+      <Navigation />
+    </HeaderWrapper>
+  </StyledHeader>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
---
+++ public-site/src/components/navigation.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+
+const navLinks = [
+  { destination: "/heroines", readable: "The Heroines" },
+  // { destination: "/vuevixens", readable: "Vue Vixens" },
+  // { destination: "/contact", readable: "Contact" },
+]
+
+const Nav = ({ className }) => (
+  <nav className={className}>
+    {navLinks.map((item, index) => (
+      <Link to={item.destination} key={index} activeClassName="active">
+        {item.readable}
+      </Link>
+    ))}
+  </nav>
+)
+
+const Navigation = styled(Nav)`
+  a {
+    color: white;
+    text-decoration: none;
+    margin-right: 20px;
+  }
+  a:hover,
+  a.active {
+    text-decoration: underline;
+  }
+`
+
+export default Navigation
