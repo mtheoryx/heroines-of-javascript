aa49a5ed9f314e7f50b54a0729034886738e9375
David Poindexter <drpoindexter@gmail.com>
2020-01-12 12:52:32 -0500

    Add large-screen 2-column view for profiles

--- public-site/src/templates/heroine.js
+++ public-site/src/templates/heroine.js
@@ -1,47 +1,73 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
+import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Social from "../components/social"
+
+const Main = styled.div`
+  padding-bottom: 20px;
+  margin-bottom: 20px;
+  border-bottom: 1px solid #bbb;
+  @media screen and (min-width: 700px) {
+    display: flex;
+    flex-direction: row;
+  }
+`
+
+const Primary = styled.div`
+  @media screen and (min-width: 700px) {
+    width: 40%;
+    padding-right: 20px;
+  }
+`
+
+const Secondary = styled.div`
+  @media screen and (min-width: 700px) {
+    width: 60%;
+  }
+`
 
 const HeroinePage = ({ data }) => {
   return (
     <Layout>
       <SEO title={data.markdownRemark.frontmatter.heroine} />
 
-      <div style={{ marginBottom: `1.45rem` }}>
-        <h1>{data.markdownRemark.frontmatter.heroine}</h1>
-        <p style={{ marginTop: "10px", fontSize: `1.5rem` }}>
-          {data.markdownRemark.frontmatter.title}
-        </p>
+      <Main>
+        <Primary>
+          <h1>{data.markdownRemark.frontmatter.heroine}</h1>
+          <p style={{ marginTop: "10px", fontSize: `1.5rem` }}>
+            {data.markdownRemark.frontmatter.title}
+          </p>
 
-        <Social links={data.markdownRemark.frontmatter.links} />
+          <Social links={data.markdownRemark.frontmatter.links} />
 
-        <div
-          style={{ marginTop: "20px" }}
-          dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
-        />
-        <Img
-          fluid={
-            data.markdownRemark.frontmatter.thumbnail.childImageSharp.fluid
-          }
-        />
+          <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+        </Primary>
+        <Secondary>
+          <Img
+            fluid={
+              data.markdownRemark.frontmatter.thumbnail.childImageSharp.fluid
+            }
+          />
 
-        <h3>Photos of the physical cards</h3>
-        <h4>Front of Card</h4>
-        <Img
-          fluid={
-            data.markdownRemark.frontmatter.cardFront.childImageSharp.fluid
-          }
-        />
-        <hr />
-        <h4>Back of Card</h4>
-        <Img
-          fluid={data.markdownRemark.frontmatter.cardBack.childImageSharp.fluid}
-        />
-      </div>
-      <hr />
+          <h3>Photos of the physical cards</h3>
+          <h4>Front of Card</h4>
+          <Img
+            fluid={
+              data.markdownRemark.frontmatter.cardFront.childImageSharp.fluid
+            }
+          />
+          <hr />
+          <h4>Back of Card</h4>
+          <Img
+            fluid={
+              data.markdownRemark.frontmatter.cardBack.childImageSharp.fluid
+            }
+          />
+        </Secondary>
+      </Main>
     </Layout>
   )
 }
